94608e367e704df9f647efaa80930aff8a6924dc
wiktor.kobiela <wuch.mail@gmail.com>
2022-12-05 21:05:51 +0100

    Add first pipelines

---
+++ docker/build_docker_image.groovy
@@ -0,0 +1,58 @@
+node {
+    def image
+    stage('Clean') {
+        println("============================================== CLEAN STAGE ==============================================")
+        cleanWs()
+    }
+    stage('Clone') {
+        println("============================================== CLONE STAGE ==============================================")
+        try {
+            sh 'git clone https://github.com/wkobiela/Dockerfiles.git'
+        } catch (Exception e) {
+            println("Exception $e")
+            error "Stage failed!"
+        }
+    }
+    stage('Build') {
+        println("============================================== BUILD STAGE ==============================================")
+        try {
+            dir("$env.WORKSPACE/Dockerfiles/$params.DockerfileDir") {
+                image = docker.build(params.ImageName)
+            }
+        } catch (Exception e) {
+            println("Exception $e")
+            error "Stage failed!"
+        }
+    }
+    stage('Test') {
+        println("============================================== TEST STAGE ===============================================")
+        try {
+            image.inside {
+                sh 'echo "Tests passed"'
+            }
+        } catch (Exception e) {
+            println("Exception $e")
+            error "Stage failed!"
+        }
+    }
+    stage('Push image') {
+        println("============================================== PUSH STAGE ===============================================")
+        try {
+            docker.withRegistry('', 'dockerhub') {
+                image.push("latest")
+            }
+        } catch (Exception e) {
+            println("Exception $e")
+            error "Stage failed!"
+        }
+    }
+    stage('Remove image') {
+        println("============================================ REMOVE STAGE ===============================================")
+        try {
+            sh "docker rmi $params.ImageName"
+        } catch (Exception e) {
+            println("Exception $e")
+            error "Stage failed!"
+        }
+    }
+}
---
+++ onnx/build_test_onnx.groovy
@@ -0,0 +1,70 @@
+node(params.NodeSelector) {
+    currentBuild.displayName = "#$env.BUILD_NUMBER node: $env.NODE_NAME"
+    stage('Clean') {
+        println("============================================== CLEAN STAGE ==============================================")
+        cleanWs()
+    }
+    docker.image('wkobiela/onnx_build_base:latest').inside(" -v /etc/localtime:/etc/localtime:ro") {
+        println("========================================== STARTING CONTAINER ===========================================")
+        stage('Info') {
+            println("============================================== INFO STAGE ===============================================")
+            println("Node selector choosen: $params.NodeSelector")
+            sh label: "Update path", script: 'export PATH="/home/jenkins/.local/bin:$PATH"'
+            println("Working dir $env.WORKSPACE")
+        }
+        stage('Install protobuf') {
+            println("========================================= INSTALL PROTOBUF STAGE =========================================")
+            try {
+               sh 'git clone https://github.com/protocolbuffers/protobuf.git'
+                dir ("$env.WORKSPACE/protobuf") {
+                    sh label: 'Checkout code', script: 'git checkout v3.20.2'
+                    sh label: 'Update submodule', script: 'git submodule update --init --recursive'
+                }
+                dir ("$env.WORKSPACE/protobuf/build_source") {
+                    sh label: "Cmake protobuf", script: "cmake ../cmake -Dprotobuf_BUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_SYSCONFDIR=/etc -DCMAKE_POSITION_INDEPENDENT_CODE=ON -Dprotobuf_BUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release"
+                    sh label: 'Make protobuf', script: 'make -j$(nproc)'
+                    sh label: 'Install protobuf', script: 'sudo make install'
+                    sh label: 'Export cmake_args', script: 'export CMAKE_ARGS="-DONNX_USE_PROTOBUF_SHARED_LIBS=OFF"'
+                }
+            } catch (Exception e) {
+                println("Exception $e")
+                error "Stage failed!"
+            }
+        }
+        stage('Clone') {
+            println("============================================== CLONE STAGE ==============================================")
+            try {
+                sh label: 'Cloning ONNX repository', script: 'git clone https://github.com/onnx/onnx.git'
+            } catch (Exception e) {
+                println("Exception $e")
+                error "Stage failed!"
+            }
+        }
+        stage('Build ONNX') {
+            println("============================================== BUILD STAGE ==============================================")
+            try {
+                dir("$env.WORKSPACE/onnx") {
+                    sh label: 'Update submodules', script: 'git submodule update --init --recursive'
+                    sh label: 'Build build_test_onnx', script: 'sudo pip install -e .'
+                }
+            }
+            catch (Exception e) {
+                println("Exception $e")
+                error "Stage failed!"
+            }
+        }
+        stage('Test ONNX') {
+            println("============================================== TEST STAGE ==============================================")
+            try {
+                dir("$env.WORKSPACE/onnx") {
+                    sh label: 'Install pytest', script: 'sudo pip install pytest nbval'
+                    sh label: 'Run tests', script: 'pytest'
+                }
+            }
+            catch (Exception e) {
+                println("Exception $e")
+                error "Stage failed!"
+            }
+        }
+    }
+}
