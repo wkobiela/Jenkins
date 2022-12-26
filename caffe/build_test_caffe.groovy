2c1bd79108de3131805b6547e5cb6d3260204822
wkobiela <12174429+wkobiela@users.noreply.github.com>
2022-12-26 20:42:20 +0100

    Fix lint in caffe pipeline (#2)
    * Fix lint v1
    * Fix class problem
    * Fix linter info

--- caffe/build_test_caffe.groovy
+++ caffe/build_test_caffe.groovy
@@ -1,21 +1,20 @@
 node(params.NodeSelector) {
     currentBuild.displayName = "#$env.BUILD_NUMBER node: $env.NODE_NAME"
 
-    def threads = sh(script: 'nproc', returnStdout: true).trim().toInteger()
+    threads = sh(script: 'nproc', returnStdout: true).trim().toInteger()
     if (threads > 4) {
         println("More than 4 threads: ${threads}")
-        threads = threads.div(2)
+        threads = threads / 2
         println("Set threads: ${threads}")
     } else if (threads == 4) {
         println("Exactly ${threads} threads.")
-        threads = 4
         println("Set threads: ${threads}")
     } else {
         println("Another variant & set threads: ${threads}")
     }
 
     stage('Clean') {
-        println("============================================== CLEAN STAGE ==============================================")
+        println('========================================= CLEAN STAGE ===============================================')
         try {
             sh label: 'Check workspace size', script: "du -sh $env.WORKSPACE"
             sh label: 'Clean workspace', script: "sudo rm -rf $env.WORKSPACE/*"
@@ -24,10 +23,10 @@ node(params.NodeSelector) {
             error "Stage failed with exception $e"
         }
     }
-    println("========================================== STARTING CONTAINER ===========================================")
-    docker.image('wkobiela/caffe_build_base:latest').inside(" -v /etc/localtime:/etc/localtime:ro") {
+    println('========================================== STARTING CONTAINER ===========================================')
+    docker.image('wkobiela/caffe_build_base:latest').inside(' -v /etc/localtime:/etc/localtime:ro') {
         stage('Clone') {
-            println("========================================== CLONE STAGE ==========================================")
+            println('========================================== CLONE STAGE ==========================================')
             try {
                 sh 'git clone https://github.com/BVLC/caffe.git'
             } catch (Exception e) {
@@ -35,19 +34,22 @@ node(params.NodeSelector) {
             }
         }
         stage('Prepare') {
-            println("============================================= PREPARE STAGE ==============================================")
+            println('======================================== PREPARE STAGE ==========================================')
             try {
-                dir ("$env.WORKSPACE/caffe/build") {
-                    sh label: 'Execute cmake command', script: 'cmake .. -DBUILD_python=OFF -DBUILD_docs=OFF -DUSE_OPENCV=OFF'
+                dir("$env.WORKSPACE/caffe/build") {
+                    sh label: 'Execute cmake command', script: 'cmake .. \
+                                                                -DBUILD_python=OFF \
+                                                                -DBUILD_docs=OFF \
+                                                                -DUSE_OPENCV=OFF'
                 }
             } catch (Exception e) {
                 error "Stage failed with exception $e"
             }
         }
         stage('Build') {
-            println("============================================= BUILD STAGE ==============================================")
+            println('======================================== BUILD STAGE ============================================')
             try {
-                dir ("$env.WORKSPACE/caffe/build") {
+                dir("$env.WORKSPACE/caffe/build") {
                     sh label: 'Execute make all command', script: "make all -j${threads}"
                     sh label: 'Execute make install command', script: 'make install'
                 }
@@ -56,7 +58,7 @@ node(params.NodeSelector) {
             }
         }
         stage('Test') {
-            println("============================================== TEST STAGE ==============================================")
+            println('========================================== TEST STAGE ===========================================')
             try {
                 dir("$env.WORKSPACE/caffe/build") {
                     sh label: 'Run tests', script: "make runtest -j${threads}"
@@ -68,7 +70,7 @@ node(params.NodeSelector) {
         }
     }
     stage('Clean') {
-        println("============================================== CLEAN STAGE ==============================================")
+        println('========================================== CLEAN STAGE ==============================================')
         try {
             sh label: 'Check workspace size', script: "du -sh $env.WORKSPACE"
             sh label: 'Clean workspace', script: "sudo rm -rf $env.WORKSPACE/*"
