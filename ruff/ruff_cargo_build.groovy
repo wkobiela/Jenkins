2df30989159e081bef770a2c0a0238e050b2d81b
wiktor.kobiela <wuch.mail@gmail.com>
2023-03-05 17:48:36 +0100

    Add a few ruff ci scripts

---
+++ ruff/ruff_cargo_build.groovy
@@ -0,0 +1,32 @@
+podTemplate(
+    containers: [
+    containerTemplate(
+        name: 'rust',
+        image: 'rust:latest',
+        command: 'sleep',
+        args: '99d',
+        resourceRequestCpu: '1',
+        resourceLimitCpu: '2',
+        resourceRequestMemory: '3Gi',
+        resourceLimitMemory: '4Gi',
+        )],
+        nodeSelector: 'bigger=true'
+        ) {
+
+    node(POD_LABEL) {
+        container('rust') {
+            stage('Cargo-build') {
+                sh 'git clone https://github.com/charliermarsh/ruff.git'
+                dir('ruff') {
+                    sh 'rustup show'
+                    sh 'cargo build --all'
+                    sh './target/debug/ruff_dev generate-all'
+                    sh '''git diff --quiet README.md || echo "::error file=README.md::This file is outdated. Run 'cargo dev generate-all'."'''
+                    sh '''git diff --quiet ruff.schema.json || echo "::error file=ruff.schema.json::This file is outdated. Run 'cargo dev generate-all'."'''
+                    sh '''git diff --exit-code -- README.md ruff.schema.json docs'''
+                }
+                sh 'rm -rf ruff'
+            }
+        }
+    }
+}
---
+++ ruff/ruff_tests_scripts.groovy
@@ -0,0 +1,31 @@
+podTemplate(
+    containers: [
+    containerTemplate(
+        name: 'rust',
+        image: 'rust:latest',
+        command: 'sleep',
+        args: '99d',
+        resourceRequestCpu: '1',
+        resourceLimitCpu: '2',
+        resourceRequestMemory: '2Gi',
+        resourceLimitMemory: '4Gi',
+        )],
+        nodeSelector: 'bigger=true'
+        ) {
+
+    node(POD_LABEL) {
+        container('rust') {
+            stage('Tests-scripts') {
+                sh 'git clone https://github.com/charliermarsh/ruff.git'
+                dir('ruff') {
+                    sh 'rustup show'
+                    sh './scripts/add_rule.py --name DoTheThing --code PLC999 --linter pylint'
+                    sh 'cargo check'
+                    sh './scripts/add_plugin.py test --url https://pypi.org/project/-test/0.1.0/ --prefix TST'
+                    sh './scripts/add_rule.py --name FirstRule --code TST001 --linter test'
+                    sh 'cargo check'
+                }
+            }
+        }
+    }
+}
