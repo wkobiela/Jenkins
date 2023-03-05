1c2f9b4dd409639cece39ef95d48b5e7c0d76d01
wiktor.kobiela <wuch.mail@gmail.com>
2023-03-05 18:41:25 +0100

    Add trigger

---
+++ ruff/trigger.groovy
@@ -0,0 +1,28 @@
+def parallelStages = [:]
+jobsArray = ['Cargo_build', 'Cargo_fmt', 'Cargo_clippy', 'Cargo_tests', 'Test_scripts']
+
+def generateStage(job) {
+    return {
+        stage("Stage: ${job}") {
+            build job: "${job}",
+            wait: true
+        }
+    }
+}
+
+jobsArray.each { job ->
+    parallelStages.put("${job}", generateStage(job))
+}
+
+pipeline {
+    agent none
+    stages {
+        stage('Run CI') {
+            steps {
+                script {
+                    parallel parallelStages
+                }
+            }
+        }
+    }
+}
