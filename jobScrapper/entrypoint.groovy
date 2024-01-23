71aae93714e0034d9b51e8f48154fc8ad33202bb
wkobiela <12174429+wkobiela@users.noreply.github.com>
2024-01-23 17:22:42 +0100

    [jobscrapper] Logic fix (#19)

--- jobScrapper/entrypoint.groovy
+++ jobScrapper/entrypoint.groovy
@@ -97,10 +97,18 @@ pipeline {
                     "Build user ID: <b>${upstreamEnv.BUILD_USER_ID}</b><br>" +
                     "Change author: <b>${upstreamEnv.CHANGE_AUTHOR}</b>"
 
-                    if ((!(whitelist.contains(upstreamEnv.CHANGE_AUTHOR)) ||
-                    !(whitelist.contains(upstreamEnv.BUILD_USER_ID))) &&
-                    !(upstreamEnv.BUILD_USER_ID == 'branchIndexing' && upstreamEnv.BUILD_USER_ID == 'null' &&
-                    upstreamEnv.CHANGE_ID == 'null')) {
+                    // If pipeline is running automatically on master
+                    if (upstreamEnv.BUILD_USER_ID.toString() == 'branchIndexing' &&
+                    upstreamEnv.BUILD_USER_ID.toString() == 'null' &&
+                    upstreamEnv.CHANGE_ID.toString() == 'null') {
+                        echo 'Build started on master by branch indexing. No need to worry.'
+                    }
+                    // if change author or build starting user is whitelisted
+                    else if (whitelist.contains(upstreamEnv.CHANGE_AUTHOR) ||
+                    whitelist.contains(upstreamEnv.BUILD_USER_ID)) {
+                        echo 'No need to add comment. User whitelisted.'
+                    // Else user not whitelister, nor master start
+                    } else {
                         echo 'Author of commit not whiltelisted or build started by scheduler.'
 
                         comment = 'Jenkins checks need to be started by whitelisted user and will appear' +
@@ -108,8 +116,6 @@ pipeline {
 
                         addComment(comment, upstreamEnv.CHANGE_ID)
                         error 'User not whitelisted.'
-                    } else {
-                        echo 'No need to add comment. User whitelisted.'
                     }
                 }
             }
