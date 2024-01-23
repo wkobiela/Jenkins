429a3b5d38fcb47fc72a6f2cb1b8a5a28418a40e
wiktor.kobiela <wuch.mail@gmail.com>
2024-01-23 16:22:06 +0100

    Next fixes

--- jobScrapper/entrypoint.groovy
+++ jobScrapper/entrypoint.groovy
@@ -97,17 +97,17 @@ pipeline {
                     "Build user ID: <b>${upstreamEnv.BUILD_USER_ID}</b><br>" +
                     "Change author: <b>${upstreamEnv.CHANGE_AUTHOR}</b>"
 
-                    if (!(whitelist.contains(upstreamEnv.CHANGE_AUTHOR)) || 
+                    if (!(whitelist.contains(upstreamEnv.CHANGE_AUTHOR)) ||
                     !(whitelist.contains(upstreamEnv.BUILD_USER_ID))) {
                         echo 'Author of commit not whiltelisted or build started by scheduler.'
-                        
+
                         comment = 'Jenkins checks need to be started by whitelisted user and will appear' +
                         ' as failed.\\nPlease wait for repo owner to start checks manually.'
-                        
+
                         addComment(comment, upstreamEnv.CHANGE_ID)
                         error 'User not whitelisted.'
                     } else {
-                            echo 'No need to add comment. User whitelisted.'
+                        echo 'No need to add comment. User whitelisted.'
                     }
                 }
             }
